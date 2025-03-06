a03f6b294968de93004d214900120a6cb81034ba
merja-shourov <merjashourov13@gmail.com>
2025-03-06 12:10:11 +0600

    fundamentals-of-solidity

---
+++ fundamentals.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract LessionOne{
+    // Variable & Data Type
+}
+
+contract LessionTwo{
+    // Array & Mapping
+}
+
+contract LessionThree{
+    // Condition & Loopos
+}
+
+contract LessionFour{
+    // Inheretence & oops
+}
